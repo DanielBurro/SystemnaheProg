5dd62c9d95a482c8f7f2e7b3b778aae649391a6c
Daniel Burrows <inf24164@lehre.dhbw-stuttgart.de>
2026-04-14 12:57:26 +0200

    Add hello_world.s to implement a simple "Hello, World!" program in RISC-V Assembly

---
+++ hello_world.s
@@ -0,0 +1,16 @@
+#
+#   data
+#
+.data
+hello_msg: .asciz "Hello, World!"
+
+#
+# code
+#
+
+.global main
+.text
+main: 
+    la a0, hello_msg    # Load address of the message into a0
+    li a7, 4               # load imm, 4 is a syscall for print_string
+    ecall 
