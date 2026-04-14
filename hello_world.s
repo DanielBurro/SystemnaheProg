8bab5f87ed12a306d71d9d154d720bdf8cae5b85
Daniel Burrows <inf24164@lehre.dhbw-stuttgart.de>
2026-04-14 13:14:44 +0200

    Fix formatting in hello_world.s

--- hello_world.s
+++ hello_world.s
@@ -11,6 +11,6 @@ hello_msg: .asciz "Hello, World!"
 .global main
 .text
 main: 
-    la a0, hello_msg    # Load address of the message into a0
-    li a7, 4               # load imm, 4 is a syscall for print_string
-    ecall 
+    la a0, hello_msg      # Load address of the message into a0
+    li a7, 4              # load imm, 4 is a syscall for print_string
+    ecall                 # make the syscall to print the message
