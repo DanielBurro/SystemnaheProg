ab82b8a1f0ed6cd7ed9edb4ce5238bdddc5d3d28
Daniel Burrows <inf24164@lehre.dhbw-stuttgart.de>
2026-04-21 12:39:01 +0200

    Add newline output after printing factorial result in RISC-V Assembly

--- fact.s
+++ fact.s
@@ -16,11 +16,15 @@ a: .word 0
 main: 
     la t0, a            # Adresse von a in t0 laden
     lw a0, 0(t0)        # Wert von a in a0 laden (Argument für fact)
-    
+
     jal fact            # fact aufrufen. Rückgabewert wird in a0 stehen
 
     li a7, 1            # Systemcall-Code 1 (print_int) in a7 laden
     ecall               # Wert in a0 auf der Konsole ausgeben
+
+    li a7, 4            # Code 4 für print_string laden
+    la a0, newline
+    ecall
 
     li a7, 10           # Systemcall-Code 10 (exit)
     ecall
@@ -33,7 +37,7 @@ fact:
 while_cond:
     bgt t1, a0, end     # Wenn i > n, springe zum Ende
 
-while_body:
+# Schleifenrumpf
     mul t0, t0, t1      # Ergebnis = Ergebnis * i
     addi t1, t1, 1      # i = i + 1
 
